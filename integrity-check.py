51a0cd176521059f8eae7c4bb67d662c87ff0358
Veljko Bozic <velko.bozic@gmail.com>
2020-09-30 17:55:08 -0400

    prototype, supports md5sum only - Veljko

--- integrity-check.py
+++ integrity-check.py
@@ -2,28 +2,52 @@
 
 import os
 import subprocess
-
+import datetime
 
 class Checker:
     def __init__(self, chksum_filename=None, chksum_hash=None, somesortofchksumrecord=None):
         self.chksum_filename = chksum_filename
         self.chksum_hash = chksum_hash
         self.somesortofchksumrecord = somesortofchksumrecord
-
+#.asc .gpg .pgp
     def md5check(self, paths):
         filenames_and_summs=dict()
         for path in paths:
             # Contains file path appended with file name
-            print("path", path)
+            print("PATH", path)
             if os.path.isfile(path):
-                print(path, "\nContains:")
-                contents = subprocess.run(['cat', path], stdout=subprocess.PIPE)
+                #contents = subprocess.run(['cat', path], stdout=subprocess.PIPE)
                 # TODO: Line bellow was an idea that might be not necessary
-                # if os.path.isfile(path-md5sums/*) and md5summs not in path/*
+                #  if os.path.isfile(path-md5sums/*) and md5summs not in path/*
                 #    if md5sum(filename) == filename_and_summs[filename]:
                 #        with open("./INTEGRITY", 'w+') as f:
                 #            f.write(filename+"  OK!"
-                print(contents.stdout.decode())
+                sum_file_name = os.path.split(path)[-1]
+                result = subprocess.Popen(['md5sum', '-c', sum_file_name], cwd=path[:-7], stdout=subprocess.PIPE)
+                out, err = result.communicate()
+                if err != None:
+                    print("ERROR:", err)
+                tmp = out.decode()
+                tmp2 = tmp.split("\n")
+                if tmp2[-1] == "":
+                    del tmp2[-1]
+
+                # This result mechanism could be written in a better fashion
+                log_dir = "log/"
+                #log_name = "integrity_check_"+datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")+".log"
+                log_name = "integrity_check.log"
+                with open(log_dir+log_name, 'a+') as f:
+                    f.write("\n\n")
+                    f.write(path[20:-7].upper().replace("/", "."))
+                    f.write("\n")
+                    for t in tmp2:
+                        f.write(t)
+                        f.write("\n")
+                f.close()
+                print("***DONE with", path, "\n\n")
+                # Alternative to Popen
+                #result = subprocess.run(['md5sum', '-c', 'md5sums'], cwd=path[:-7], stdout=subprocess.PIPE)
+
             else:
                 print(path, "is not a valid md5 check sum file!")
 
